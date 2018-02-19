882487b8a918d5f132cc8a72937fcb702fff3178
mennafouad <36284455+mennafouad@users.noreply.github.com>
2018-02-19 15:07:10 -0800

    test

---
+++ q2.ino
@@ -0,0 +1,36 @@
+int yellow =4;
+int green=5;
+int red=6;
+int blue=7;
+void setup() {
+  pinMode (yellow,OUTPUT);
+  pinMode (green,OUTPUT);
+  pinMode (red,OUTPUT);
+  pinMode (blue,OUTPUT);
+  
+
+}
+
+void loop() {
+  digitalWrite (yellow,HIGH);
+  digitalWrite (green,LOW);
+  digitalWrite (red,LOW);
+  digitalWrite (blue,LOW);
+  delay(1000);
+  digitalWrite (yellow,LOW);
+ digitalWrite (green,HIGH);
+  digitalWrite (red,LOW);
+  digitalWrite (blue,LOW);
+  delay(1000);
+ 
+   digitalWrite (yellow,LOW);
+ digitalWrite (green,LOW);
+  digitalWrite (red,HIGH);
+  digitalWrite (blue,LOW);
+  delay(1000);
+   digitalWrite (yellow,LOW);
+ digitalWrite (green,LOW);
+  digitalWrite (red,LOW);
+  digitalWrite (blue,HIGH);
+  delay(1000);
+}
