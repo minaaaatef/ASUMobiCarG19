f5342bd0446fbe549012d6a627d21f3f30aa27f5
marygirgiszaky <36311441+marygirgiszaky@users.noreply.github.com>
2018-02-22 20:13:15 +0200

    Add files via upload

---
+++ Led.ino
@@ -0,0 +1,18 @@
+int i ;
+void setup() 
+{
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+}
+
+void loop() {
+  for (i=4;i<8;i++)
+  {
+ digitalWrite(i,HIGH);
+ delay(1000);
+ digitalWrite(i,LOW);
+ delay(1000);
+  }
+}
