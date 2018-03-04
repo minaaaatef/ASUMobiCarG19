a25ba114bd78287191d49dd7178f3fc6ed325884
MarianRamsesFawzy <36311300+MarianRamsesFawzy@users.noreply.github.com>
2018-03-04 19:28:32 +0200

    Add files via upload

---
+++ Car Tasks/stopcar.ino
@@ -0,0 +1,17 @@
+
+// stopcar
+const int pin2,pin3;
+
+void setup() {
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+  
+
+}
+
+void stopcar() {
+
+  digitalWrite(pin2,0);
+  digitalWrite(pin3,0);
+
+}
