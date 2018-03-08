71cdbcdf2beb7ba4216ce6755057213e85e391e2
mennafouad <36284455+mennafouad@users.noreply.github.com>
2018-03-08 12:32:28 -0800

    .

---
+++ Car Tasks/ultersonic.ino
@@ -0,0 +1,26 @@
+int TriggerPin =9;
+int EchoPin =10;
+float distance;
+long period;
+
+void setup() {
+  pinMode (TriggerPin,OUTPUT);
+  pinMode (EchoPin,INPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(TriggerPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(TriggerPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin,LOW);
+  period= pulseIn(EchoPin,HIGH);
+  distance=period*0.034/2;
+  Serial.println(distance);
+  
+  // put your main code here, to run repeatedly:
+
+}
