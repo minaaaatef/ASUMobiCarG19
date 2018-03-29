665fe73be882b8b97ded184cbc177562570a3355
mennafouad <36284455+mennafouad@users.noreply.github.com>
2018-03-29 01:34:30 -0700

    .

---
+++ Sheets/lcd2.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+LiquidCrystal LCD1(8,9,4,5,6,7);
+float temp;
+const int LM =A0;
+void setup() {
+  LCD1.begin(16,2);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  temp= analogRead(LM);
+  temp = (5*temp*100)/1024;
+  LCD1.setCursor(6,0);
+  LCD1.print(temp);
+  
+  // put your main code here, to run repeatedly:
+
+}
