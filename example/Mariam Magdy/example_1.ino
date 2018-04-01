d0efb618a8863f424d55f906d46976cde1f3a033
MariamMagdy <36310466+MariamMagdy@users.noreply.github.com>
2018-04-01 20:56:25 +0300

    Add files via upload

---
+++ example/Mariam Magdy/example_1.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (13,OUTPUT);
+pinMode (2,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(2)==1)
+digitalWrite(13,HIGH);
+else
+digitalWrite(13,LOW);
+}
