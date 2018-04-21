92498d5e9e562943bb420fd4997a37e7ff486030
marygirgiszaky <36311441+marygirgiszaky@users.noreply.github.com>
2018-04-21 02:58:49 +0200

    example/computer controlled fan

---
+++ computer_controlled_fan.ino
@@ -0,0 +1,19 @@
+ int motorpin = 11 ;
+void setup() {
+ pinMode ( motorpin ,OUTPUT);
+ analogWrite(motorpin,0);
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available())
+{
+ char ch = Serial.read();
+ if(ch>='0'&&ch<='9')
+ {
+  int speed =ch-'0';
+  analogWrite(motorpin,speed*28);
+ }
+}
+}
