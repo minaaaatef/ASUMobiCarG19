aa10b63a03124418dc2e3421f95ca47cacc31017
marygirgiszaky <36311441+marygirgiszaky@users.noreply.github.com>
2018-04-26 22:11:42 +0200

    example/ oscillioscope

---
+++ oscillioscope.ino
@@ -0,0 +1,13 @@
+int channel_A_pin = 0 ;
+void setup() {
+ Serial.begin(115200);
+
+}
+
+void loop() {
+int value = analogRead (channel_A_pin); 
+value = ( value >> 2)& 0xFF ;
+Serial.print(value , BYTE );
+delayMicroseconds(100); 
+
+}
